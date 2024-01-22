881d695b3aa1b6804639e0ca8d5f188255980005
Raffael-TestGit <148634688+Raffael-TestGit@users.noreply.github.com>
2024-01-22 11:11:55 +0100

    added stats

---
+++ src/components/Menu.vue
@@ -0,0 +1,93 @@
+<template>
+<div>
+    <v-container v-if="!statsDisplayed">
+      <div class="center"><h1>Neunerln</h1><hr><br/></div>
+      <v-row justify="center">
+        <v-col cols="4">
+          <v-btn 
+            variant="outlined"
+            href=""
+            block
+            size="x-large"
+          >
+            Spiel starten
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="4">
+          <v-btn 
+            variant="outlined"
+            id="viewStats"
+            href=""
+            block
+            size="x-large"
+            @click="showStats"
+          >
+            Statistiken ansehen
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="4">
+          <v-btn 
+            variant="outlined"
+            id="endgame"
+            block
+            size="x-large"
+            @click="endGame"
+          >
+            Spiel beenden
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <StatInterface v-if="statsDisplayed" @backToMenu="backToMenu"/>
+</div>
+  </template>
+    
+  <script>
+    import StatInterface from './StatInterface.vue';
+
+    export default {
+      name: "Menu",
+
+      components: {
+        StatInterface
+      },
+      props: {
+        cardRef: String,
+      },
+  
+      data: () => ({
+        model: null,
+        statsDisplayed: false,
+      }),
+  
+      methods: {
+        showStats () {
+          if (this.statsDisplayed === true) {
+            this.statsDisplayed = false
+          } else {
+            this.statsDisplayed = true
+          }
+        },
+        endGame(){
+          close();
+        },
+        backToMenu(){
+          this.statsDisplayed = false;
+        }
+      }
+    }
+  </script>
+  
+  <style scoped>
+    .playing-card {
+      height: 300px;
+    }
+
+    .center{
+      text-align: center;
+    }
+  </style>
---
+++ src/components/StatInterface.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-table theme="dark">
+      <thead>
+        <tr>
+          <th class="text-left">
+          Bezeichnung
+          </th>
+          <th class="text-left">
+          Wert
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in stats"
+          :key="item.bezeichnung"
+        >
+          <td>{{ item.bezeichnung }}</td>
+          <td>{{ item.wert }}</td>
+        </tr>
+        <button class="btn" @click="backToMenu">Zurück zum Menü</button>
+      </tbody>
+    </v-table>
+  </template>
+    
+  <script>
+    let playedGames = 15;
+    let wonGames = 12;
+    export default {
+      data () {       
+        return {
+          stats: [
+            {
+              bezeichnung: 'Gespielte Spiele',
+              wert: playedGames,
+            },
+            {
+              bezeichnung: 'Gewonnene Spiele',
+              wert: wonGames,
+            },
+            {
+              bezeichnung: 'Siegesquote',
+              wert: Math.round(wonGames/playedGames*100) + '%',
+            },
+          ],
+        }
+      },
+        methods: {
+          backToMenu(){
+            this.$emit('backToMenu');
+          }
+        }
+    }
+  </script>
+  
+  <style scoped>
+    .btn{
+      font-size: 150%;
+      margin-left: 50%
+    }
+  </style>
