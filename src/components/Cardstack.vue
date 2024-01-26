b18fe8d541ee61bb9a2d713ee29623931614ce79
Raffael-TestGit <148634688+Raffael-TestGit@users.noreply.github.com>
2024-01-26 10:26:54 +0100

    worked on Cardstack

--- src/components/Cardstack.vue
+++ src/components/Cardstack.vue
@@ -1,52 +1,57 @@
 <template>
-    <v-col
-      class="playing-card"
-    >
-      <v-img
-        src="../assets/cards/Karten_Rückseite.png"
-        @click="drawCard"
-      ></v-img>
-    </v-col>
-  </template>
-    
-  <script>
-    let player1_hand = []
-    let player2_hand = []
-    let ablagestapel = []
-    export default {
-      name: "PlayingCard",
-      props: {
-        cards: Array
-      },
-  
-      data: () => ({
-        model: null,
-      }),
+  <v-col class="playing-card">
+    <v-img
+      id="cardstack"
+      alt="Kartenruecken"
+      :src="cardstackImage"
+      @click="drawCard"
+    ></v-img>
+  </v-col>
+</template>
 
-      created(){
-          let index = Math.floor(Math.random()*32);
-          const randomCard = this.cards[index];
-          ablagestapel.push(randomCard);
-          this.cards.splice(index, 1);
-      },
+<script>
 
-      methods: {
-        drawCard () {
-          let index = Math.floor(Math.random()*32);
-          const randomCard = this.cards[index];
-          player1_hand.push(randomCard);
-          this.cards.splice(index, 1);
-          player1_hand.forEach(element => {
-            console.log(element);
-          });
+export default {
+  name: "PlayingCard",
+  props: {
+    cards: Array
+  },
+
+  data: () => ({
+    model: null,
+    player1_hand: [],
+    player2_hand:[],
+    ablagestapel: [],
+    cardstackImage: "src/assets/cards/Karten_Rückseite.png"
+  }),
+
+  created() {
+    let index = Math.floor(Math.random() * this.cards.length);
+    const randomCard = this.cards[index];
+    this.ablagestapel.push(randomCard);
+    this.cards.splice(index, 1);
+  },
+
+  methods: {
+    drawCard() {
+      if (this.cards.length > 0) {
+        let index = Math.floor(Math.random() * this.cards.length);
+        const randomCard = this.cards[index];
+        this.player1_hand.push(randomCard);
+        this.cards.splice(index, 1);
+        this.player1_hand.forEach(element => {});
+        console.log(this.cards.length);
+        if (this.cards.length <= 0) {
+          this.cardstackImage = "src/assets/cards/Leerer_Stapel.png";
         }
       }
-      
     }
-  </script>
-  
-  <style scoped>
-    .playing-card {
-      height: 300px;
-    }
-  </style>
+  }
+};
+</script>
+
+<style scoped>
+.playing-card {
+  height: 300px;
+}
+</style>
